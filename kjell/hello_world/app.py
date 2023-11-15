89ced8cdd3a3fb240a00d7115f0cd5b3f5935262
Sebastian <sebastian.nordby.bjornstad@wlcom.no>
2023-11-15 12:16:50 +0100

    Oppgave1A: Lagt til pyhon kode for kjell

---
+++ kjell/hello_world/app.py
@@ -0,0 +1,42 @@
+import json
+import boto3
+import os
+
+# Oppgave 1A
+s3_client = boto3.client('s3', region_name='eu-west-1')
+rekognition_client = boto3.client('rekognition', region_name='eu-west-1')
+
+# Hent miljøvariabel <BUCKET_NAME>
+try:
+    BUCKET_NAME = os.environ['BUCKET_NAME']
+except KeyError:
+    raise ValueError("The environment variable <BUCKET_NAME> must be provided.") 
+
+def lambda_handler(event, context):
+    # List all objects in the S3 bucket
+    paginator = s3_client.get_paginator('list_objects_v2')
+    rekognition_results = []  # Store the results
+
+    for page in paginator.paginate(Bucket=BUCKET_NAME):
+        for obj in page.get('Contents', []):
+            # Perform PPE detection using Rekognition
+            rekognition_response = rekognition_client.detect_protective_equipment(
+                Image={
+                    'S3Object': {
+                        'Bucket': BUCKET_NAME,
+                        'Name': obj['Key']
+                    }
+                },
+                SummarizationAttributes={
+                    'MinConfidence': 80,  # Confidence level threshold
+                    'RequiredEquipmentTypes': ['FACE_COVER']
+                }
+            )
+            rekognition_results.append(rekognition_response)
+
+    return {
+        "statusCode": 200,
+        "body":  json.dumps(rekognition_results),
+    }
+
+print(lambda_handler(None, None))
