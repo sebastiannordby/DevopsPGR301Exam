321e64d1dc5f3a6b34823acd1f3c9cbc0caf49c8
Sebastian <sebastian.nordby.bjornstad@wlcom.no>
2023-11-15 19:22:06 +0100

    Oppgave3A: Fjernet hardkoding av diverse felter og gått over til å bruke variabler. Har også redusert CPU til 256 og minne til 1024

--- infra/main.tf
+++ infra/main.tf
@@ -1,27 +1,58 @@
+variable "apprunner_service_name" {
+  description = "Name of the AppRunner service"
+  type = string
+}
+
+variable "ecr_repository" {
+  description = "URI to ECR repository"
+  type = string
+}
+
+variable "iam_policy_name" {
+  description = "IAM Policy Name"
+  type = string
+}
+
+variable "apprunner_policy_name" {
+  description = "AppRunner Instance Policy Name"
+  type = string
+}
+
+variable "apprunner_container_port" {
+  description = "Container port number"
+  type = number
+  default = 8080
+}
+
 resource "aws_apprunner_service" "service" {
-  service_name = "kjell-is-king"
+  service_name = var.apprunner_service_name
 
   instance_configuration {
     instance_role_arn = aws_iam_role.role_for_apprunner_service.arn
+    cpu = 256
+    memory = 1024
   }
 
   source_configuration {
     authentication_configuration {
       access_role_arn = "arn:aws:iam::244530008913:role/service-role/AppRunnerECRAccessRole"
     }
+
     image_repository {
       image_configuration {
-        port = "8080"
+        port = var.apprunner_container_port
       }
-      image_identifier      = "244530008913.dkr.ecr.eu-west-1.amazonaws.com/kjell:latest"
+
+      image_identifier = "${var.ecr_repository}:latest"
       image_repository_type = "ECR"
     }
+
     auto_deployments_enabled = true
   }
 }
 
 resource "aws_iam_role" "role_for_apprunner_service" {
-  name               = "kjell-role-thingy"
+  name = var.apprunner_policy_name
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
@@ -31,7 +62,7 @@ data "aws_iam_policy_document" "assume_role" {
     effect = "Allow"
 
     principals {
-      type        = "Service"
+      type = "Service"
       identifiers = ["tasks.apprunner.amazonaws.com"]
     }
 
@@ -41,32 +72,31 @@ data "aws_iam_policy_document" "assume_role" {
 
 data "aws_iam_policy_document" "policy" {
   statement {
-    effect    = "Allow"
-    actions   = ["rekognition:*"]
+    effect = "Allow"
+    actions = ["rekognition:*"]
     resources = ["*"]
   }
   
   statement  {
-    effect    = "Allow"
-    actions   = ["s3:*"]
+    effect = "Allow"
+    actions = ["s3:*"]
     resources = ["*"]
   }
 
   statement  {
-    effect    = "Allow"
-    actions   = ["cloudwatch:*"]
+    effect = "Allow"
+    actions = ["cloudwatch:*"]
     resources = ["*"]
   }
 }
 
 resource "aws_iam_policy" "policy" {
-  name        = "kjell-apr-policy-thingy"
-  description = "Policy for apprunner instance I think"
-  policy      = data.aws_iam_policy_document.policy.json
+  name = var.iam_policy_name
+  description = "Policy for AppRunner Instance"
+  policy = data.aws_iam_policy_document.policy.json
 }
 
-
 resource "aws_iam_role_policy_attachment" "attachment" {
-  role       = aws_iam_role.role_for_apprunner_service.name
+  role = aws_iam_role.role_for_apprunner_service.name
   policy_arn = aws_iam_policy.policy.arn
 }
