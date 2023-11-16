405ba622c39c4ab72812dd5a2eac69bbc329b03e
Sebastian Nordby <sebastian.nordby.bjornstad@wlcom.no>
2023-11-16 16:22:42 +0100

    Oppgave4: Test om Terraform går igjennom uten miljøvariabler

--- infra/main.tf
+++ infra/main.tf
@@ -15,12 +15,6 @@ resource "aws_apprunner_service" "service" {
     image_repository {
       image_configuration {
         port = var.apprunner_container_port
-        runtime_environment_variables = {
-          "MANAGEMENT_METRICS_EXPORT_CLOUDWATCH_NAMESPACE" = var.cloudwatch_namespace
-          "MANAGEMENT_METRICS_EXPORT_CLOUDWATCH_BATCHSIZE"= tostring(var.cloudwatch_batch_size)
-          "MANAGEMENT_METRICS_EXPORT_CLOUDWATCH_STEP" = var.cloudwatch_step
-          "MANAGEMENT_METRICS_EXPORT_CLOUDWATCH_ENABLED" = tostring(var.cloudwatch_enabled)
-        }
       }
 
       image_identifier = var.ecr_repository_uri
