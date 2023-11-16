f1f830fba03426d5746bd19876cdf7ef3d792b43
Sebastian <sebastian.nordby.bjornstad@wlcom.no>
2023-11-16 12:40:39 +0100

    Oppgave4: Konverter runtime_environment_variables til et argument i steden for blokk?

--- infra/main.tf
+++ infra/main.tf
@@ -15,8 +15,7 @@ resource "aws_apprunner_service" "service" {
     image_repository {
       image_configuration {
         port = var.apprunner_container_port
-
-        runtime_environment_variables {
+        runtime_environment_variables = {
           MANAGEMENT_METRICS_EXPORT_CLOUDWATCH_NAMESPACE = var.cloudwatch_namespace
           MANAGEMENT_METRICS_EXPORT_CLOUDWATCH_BATCHSIZE = tostring(var.cloudwatch_batch_size)
           MANAGEMENT_METRICS_EXPORT_CLOUDWATCH_STEP = var.cloudwatch_step
